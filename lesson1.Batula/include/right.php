26f03dc50a42df452b0485c867e198b40bc4e0c6
tul6skiu <36962725+tul6skiu@users.noreply.github.com>
2018-03-27 18:36:56 +0300

    files

---
+++ lesson1.Batula/include/right.php
@@ -0,0 +1,12 @@
+  <div class="col-md-3">
+            <h3 class="my-4">Категории</h3>
+              <div class="list-group">
+                <a href="#" class="list-group-item list-group-item-action active">
+                  Категория 1
+                </a>
+                <a href="#" class="list-group-item list-group-item-action">Категория 2</a>
+                <a href="#" class="list-group-item list-group-item-action">Категория 3</a>
+                <a href="#" class="list-group-item list-group-item-action">Категория 4</a>
+                <a href="#" class="list-group-item list-group-item-action disabled">Категория 5</a>
+              </div>
+        </div>
---
+++ lesson1.Batula/index.php
@@ -0,0 +1,59 @@
+<!DOCTYPE html>
+<html lang="en">
+
+ <head>
+
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="">
+
+    <title>Blog</title>
+
+    
+    <link href="vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+
+ 
+    <link href="css/main.css" rel="stylesheet">
+
+  </head>
+
+  <body>
+  
+
+  <?php 
+
+   include 'include/nav.php';
+ 
+  ?>
+
+
+	  
+   <?php
+    include 'include/content.php';
+
+   ?>
+
+   
+   <?php
+   
+    include 'include/right.php';
+
+   ?>
+        <!-- Sidebar -->
+  <?php
+   
+    include 'include/footer.php';
+
+   ?>  
+
+
+
+
+    <!-- Bootstrap core JavaScript -->
+    <script src="vendor/jquery/jquery.min.js"></script>
+    <script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+
+  </body>
+
+</html>
