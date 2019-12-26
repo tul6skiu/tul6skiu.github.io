dd4423ab34dea500f564f6214f78069c24174673
tul6skiu <36962725+tul6skiu@users.noreply.github.com>
2019-12-26 11:03:18 +0300

    Update js2.js

--- test2/js2.js
+++ test2/js2.js
@@ -1,5 +1,5 @@
 function soundClick() {
   var audio = new Audio(); // Создаём новый элемент Audio
-  audio.src='Liquid Assassin-Far Away-kissvk.com.mp3'; // Указываем путь к звуку "клика"
+  audio.src='333.mp3'; // Указываем путь к звуку "клика"
   audio.autoplay = true; // Автоматически запускаем
 }
