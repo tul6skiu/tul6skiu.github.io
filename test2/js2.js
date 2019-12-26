085eda1b099e6daa5f65ce4a8f11cd3fdbc122ea
tul6skiu <36962725+tul6skiu@users.noreply.github.com>
2019-12-26 11:54:38 +0300

    Update js2.js

--- test2/js2.js
+++ test2/js2.js
@@ -1,5 +1,5 @@
 function soundClick() {
   var audio = new Audio(); // Создаём новый элемент Audio
-  audio.src='333.mp3'; // Указываем путь к звуку "клика"
+  audio.src='444.mp3'; // Указываем путь к звуку "клика"
   audio.autoplay = true; // Автоматически запускаем
 }
